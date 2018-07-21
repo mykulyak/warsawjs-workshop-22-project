221709d637d5c9a02f70ee7ad321664b020691d6
Andriy Mykulyak <mykulyak@gmail.com>
2018-07-21 20:45:51 +0200

    Implement CurrencyExchange.constructor and tests

---
+++ src/CurrencyExchange.js
@@ -0,0 +1,24 @@
+export default class CurrencyExchange {
+  constructor(exchangeRates, options) {
+    if (!Array.isArray(exchangeRates) || exchangeRates.length === 0) {
+      throw new Error('Invalid exchange rates');
+    }
+    const { names, rates } = exchangeRates.reduce((accum, { code, buy, sell }) => {
+      if (!code || buy <= 0 || sell <= 0 || buy > sell) {
+        throw new Error('Invalid currency data');
+      }
+      accum.names.push(code);
+      accum.rates[code] = { buy, sell };
+      return accum;
+    }, { names: [], rates: {} });
+    this.names = names;
+    this.rates = rates;
+
+    const { buyFee, sellFee } = Object.assign({ buyFee: 0.01, sellFee: 0.01 }, options);
+    if (buyFee < 0.01 || sellFee < 0.01) {
+      throw new Error('Invalid service fees');
+    }
+    this.buyFee = buyFee;
+    this.sellFee = sellFee;
+  }
+}
---
+++ src/CurrencyExchange.test.js
@@ -0,0 +1,80 @@
+import CurrencyExchange from './CurrencyExchange';
+
+test('CurrencyExchange powinien być klasą', () => {
+  expect(CurrencyExchange.toString()).toMatch(/class/);
+});
+
+describe('constructor', () => {
+  test('rzuca wyjątek jeśli nie zapodano mu tablicy z danymi', () => {
+    expect(() => {
+      new CurrencyExchange();
+    }).toThrow();
+    expect(() => {
+      new CurrencyExchange(null);
+    }).toThrow();
+    expect(() => {
+      new CurrencyExchange({});
+    }).toThrow();
+  });
+
+  test('rzuca wyjątek jeśli zapodano mu pustą tablicę', () => {
+    expect(() => {
+      new CurrencyExchange([]);
+    }).toThrow();
+  });
+
+  test('rzuca wyjątek jeśli waluta nie nieprawidłowy kod', () => {
+    expect(() => {
+      new CurrencyExchange([
+        { code: null, buy: 3, sell: 4 },
+      ]);
+    }).toThrow();
+  });
+
+  test('rzuca wyjątek jeśli kurs nie jest dodatni', () => {
+    expect(() => {
+      new CurrencyExchange([
+        { code: 'USD', buy: 3, sell: 0 }
+      ]);
+    }).toThrow();
+    expect(() => {
+      new CurrencyExchange([
+        { code: 'USD', buy: -1, sell: 4 }
+      ]);
+    }).toThrow();
+    expect(() => {
+      new CurrencyExchange([
+        { code: 'USD', buy: null, sell: 4 }
+      ]);
+    }).toThrow();
+  });
+
+  test('rzuca wyjątek jeśli kurs kupna jest większy niż kurs sprzedaży', () => {
+    expect(() => {
+      new CurrencyExchange([
+        { code: 'USD', buy: 3, sell: 2 }
+      ]);
+    }).toThrow();
+  });
+
+  test('rzuca wyjątek jeśli kwota prowizji nie jest dodatnia', () => {
+    expect(() => {
+      new CurrencyExchange([
+        { code: 'USD', buy: 3, sell: 4 },
+      ], { buyFee: -1, sellFee: 2 });
+    }).toThrow();
+    expect(() => {
+      new CurrencyExchange([
+        { code: 'USD', buy: 3, sell: 4 },
+      ], { buyFee: 1, sellFee: -2 });
+    }).toThrow();
+  });
+
+  test('jeśli wartości prowizji nie zostały podane, są one zerowe 0.01', () => {
+    const exchanger = new CurrencyExchange([
+      { code: 'USD', buy: 3, sell: 4 }
+    ], {});
+    expect(exchanger.buyFee).toBeCloseTo(0.01, 2);
+    expect(exchanger.sellFee).toBeCloseTo(0.01, 2);
+  });
+});
